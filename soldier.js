8b5b70c49197cb8f0c8ca4c46067480eac5777f6
Daim <muslim_abdulov@mail.ru>
2023-01-05 11:49:47 -0800

    Создан объект - солдат. Добавлены свойства и методы

---
+++ soldier.js
@@ -0,0 +1,16 @@
+let soldier = {
+    name: 'John',
+    heal: 10,
+    weapon: {
+        name: 'M4A1 carbine',
+        bullets: 30
+    },
+    supplies: 3,
+
+    fire() {},
+
+    reload() {},
+
+    wound() {},
+
+}
